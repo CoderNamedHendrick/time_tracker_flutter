3d63f01e5b50082b881dc8049fbbeeb8f6cd5098
Hendrick <sebastinesoacatp@gmail.com>
2021-03-01 14:39:58 +0100

    Building the signIn page layout

--- lib/app/signin/signin_page.dart
+++ lib/app/signin/signin_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter/common_widgets/custom_raised_button.dart';
+import 'package:time_tracker_flutter/common_widgets/sign_in_button.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -26,14 +27,38 @@ class SignInPage extends StatelessWidget {
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600),
           ),
-          SizedBox(height: 8.0),
-          CustomRaisedButton(
-            child: Text(
-              "Sign in with Google",
-              style: TextStyle(fontSize: 15, color: Colors.black87),
-            ),
+          SizedBox(height: 48.0),
+          SignInButton(
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
             color: Colors.white,
-            borderRadius: 4.0,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0,),
+          SignInButton(
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0,),
+          SignInButton(
+            text: 'Sign in with email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0,),
+          Text(
+            "or",
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8.0,),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
             onPressed: () {},
           ),
         ],
