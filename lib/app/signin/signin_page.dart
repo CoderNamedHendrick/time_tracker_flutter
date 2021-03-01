2b5b4d50f889678ef39b08330259b29d37614e95
Hendrick <sebastinesoacatp@gmail.com>
2021-03-01 13:51:02 +0100

    Building the signIn page layout

--- lib/app/signin/signin_page.dart
+++ lib/app/signin/signin_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatelessWidget{
+class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,19 +8,37 @@ class SignInPage extends StatelessWidget{
         title: Text("Time Tracker"),
         elevation: 2.0,
       ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              color: Colors.orange,
-              child: SizedBox(
-                height: 100,
-              ),
-            )
-          ],
-        ),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+          SizedBox(height: 8.0,),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+          SizedBox(height: 8.0,),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+        ],
       ),
     );
   }
