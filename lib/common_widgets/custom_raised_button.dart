95d5dec1f4dc5d8caa23d561c39f532bed3cd7de
Hendrick <sebastinesoacatp@gmail.com>
2021-03-01 15:14:05 +0100

    Cleaning the buttons

--- lib/common_widgets/custom_raised_button.dart
+++ lib/common_widgets/custom_raised_button.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CustomRaisedButton extends StatelessWidget{
-  CustomRaisedButton({
-    this.child,
-    this.color,
-    this.borderRadius: 2.0,
-    this.height: 50.0,
-    this.onPressed
-});
+class CustomRaisedButton extends StatelessWidget {
+  CustomRaisedButton(
+      {this.child,
+      this.color,
+      this.borderRadius: 2.0,
+      this.height: 50.0,
+      this.onPressed})
+      : assert(borderRadius != null);
+
   final Widget child;
   final Color color;
   final double borderRadius;
@@ -23,8 +24,8 @@ class CustomRaisedButton extends StatelessWidget{
         color: color,
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(
-              Radius.circular(borderRadius),)
-        ),
+          Radius.circular(borderRadius),
+        )),
         onPressed: onPressed,
       ),
     );
