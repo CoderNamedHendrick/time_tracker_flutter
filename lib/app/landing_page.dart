47967f471581d92b23b1acc822f462629f350ac5
Hendrick <sebastinesoacatp@gmail.com>
2021-03-02 21:23:08 +0100

    implementing sign-out functionality

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter/app/home_page.dart';
 import 'package:time_tracker_flutter/app/signin/signin_page.dart';
 
 class LandingPage extends StatefulWidget{
@@ -24,6 +25,6 @@ class _LandingPageState extends State<LandingPage>{
       return SignInPage(
         onSignIn: _updateUser,
       );
-    return Container();
+    return HomePage();
   }
 }
