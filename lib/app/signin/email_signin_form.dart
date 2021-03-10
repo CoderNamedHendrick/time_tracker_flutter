e77c5d8e957fabc5505da9d406f8d440b5637803
Hendrick <sebastinesoacatp@gmail.com>
2021-03-10 23:16:15 +0100

    Email/password login: Adding functionality

--- lib/app/signin/email_signin_form.dart
+++ lib/app/signin/email_signin_form.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter/common_widgets/form_submit_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    List<Widget> _buildChildren(){
+    List<Widget> _buildChildren() {
       return [
         TextField(
           decoration: InputDecoration(
@@ -12,17 +12,28 @@ class EmailSignInForm extends StatelessWidget {
             hintText: 'test@test.com',
           ),
         ),
+        SizedBox(height: 8.0),
         TextField(
-          decoration: InputDecoration(
-            labelText: 'Password'
-          ),
+          decoration: InputDecoration(labelText: 'Password'),
           obscureText: true,
         ),
+        SizedBox(height: 8.0),
+        FormSubmitButton(
+          text: 'Sign in',
+          onPressed: () {},
+        ),
+        SizedBox(height: 8.0),
+        FlatButton(
+          onPressed: () {},
+          child: Text('Need an account? Register'),
+        )
       ];
     }
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         children: _buildChildren(),
       ),
