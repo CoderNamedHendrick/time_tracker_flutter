092dc610b4e5c243c5772d39a2cdcf579b4e5b5f
Hendrick <sebastinesoacatp@gmail.com>
2021-03-10 23:03:31 +0100

    Email/password login: Creating the signin page

--- lib/app/signin/email_signin_page.dart
+++ lib/app/signin/email_signin_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter/app/signin/email_signin_form.dart';
 
-class EmailSignInPage extends StatelessWidget{
+class EmailSignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,12 +10,13 @@ class EmailSignInPage extends StatelessWidget{
         title: Text("Time Tracker"),
         elevation: 2.0,
       ),
-      body: _buildContent(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          child: EmailSignInForm(),
+        ),
+      ),
       backgroundColor: Colors.grey[200],
     );
-  }
-
-  Widget _buildContent() {
-    return Container();
   }
 }
