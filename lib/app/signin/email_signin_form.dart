d33f5ae6b5a91cbb6beb1187e627fd583282a719
Hendrick <sebastinesoacatp@gmail.com>
2021-03-10 23:07:34 +0100

    Email/password login: Creating the signin page

--- lib/app/signin/email_signin_form.dart
+++ lib/app/signin/email_signin_form.dart
@@ -3,6 +3,29 @@ import 'package:flutter/material.dart';
 class EmailSignInForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    List<Widget> _buildChildren(){
+      return [
+        TextField(
+          decoration: InputDecoration(
+            labelText: 'Email',
+            hintText: 'test@test.com',
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            labelText: 'Password'
+          ),
+          obscureText: true,
+        ),
+      ];
+    }
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
   }
 }
