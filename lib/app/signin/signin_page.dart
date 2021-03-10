b6165b30b26ee7aa875bb627e18dbcebce80cfe3
Hendrick <sebastinesoacatp@gmail.com>
2021-03-10 18:34:02 +0100

    Improving the google sign in flow

--- lib/app/signin/signin_page.dart
+++ lib/app/signin/signin_page.dart
@@ -9,7 +9,15 @@ class SignInPage extends StatelessWidget {
 
   Future<void> _signInAnonymously() async{
     try {
-      final user = await auth.signInAnonymously();
+      await auth.signInAnonymously();
+    } catch(e){
+      print(e.toString());
+    }
+  }
+
+  Future<void> _signInWithGoogle() async{
+    try {
+      await auth.signInWithGoogle();
     } catch(e){
       print(e.toString());
     }
@@ -43,7 +51,7 @@ class SignInPage extends StatelessWidget {
             assetName: 'images/google-logo.png',
             text: 'Sign in with Google',
             textColor: Colors.black87,
-            onPressed: () {},
+            onPressed: _signInWithGoogle,
           ),
           SizedBox(height: 8.0,),
           SocialSignInButton(
