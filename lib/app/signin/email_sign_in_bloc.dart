f3320420f365e5f84f57ae2e152b1452e002bdf2
Hendrick <sebastinesoacatp@gmail.com>
2021-04-14 12:58:13 +0100

    Moving the sign in form widget to using bloc: Fixing streams for the bloc

--- lib/app/signin/email_sign_in_bloc.dart
+++ lib/app/signin/email_sign_in_bloc.dart
@@ -2,11 +2,32 @@ import 'dart:async';
 
 import 'package:time_tracker_flutter/app/signin/email_sign_in_model.dart';
 
-class EmailSignInBloc{
-  final StreamController<EmailSignInModel> _modelController = StreamController<EmailSignInModel>();
+class EmailSignInBloc {
+  final StreamController<EmailSignInModel> _modelController =
+      StreamController<EmailSignInModel>();
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
 
-  void dispose(){
+  EmailSignInModel _model = EmailSignInModel();
+  void dispose() {
     _modelController.close();
+  }
+
+  void updateWith({
+    String email,
+    String password,
+    EmailSignInFormType formType,
+    bool isLoading,
+    bool submitted,
+  }) {
+    // update model
+    _model = _model.copyWith(
+      email: email,
+      password: password,
+      formType: formType,
+      isLoading: isLoading,
+      submitted: submitted,
+    );
+    // add updated model to _modelController
+    _modelController.add(_model);
   }
 }
--- lib/app/signin/email_sign_in_model.dart
+++ lib/app/signin/email_sign_in_model.dart
@@ -4,7 +4,7 @@ class EmailSignInModel {
   EmailSignInModel({
     this.email = '',
     this.password = '',
-    this.formType =  EmailSignInFormType.signIn,
+    this.formType = EmailSignInFormType.signIn,
     this.isLoading = false,
     this.submitted = false,
   });
@@ -13,4 +13,20 @@ class EmailSignInModel {
   final EmailSignInFormType formType;
   final bool isLoading;
   final bool submitted;
+
+  EmailSignInModel copyWith({
+    String email,
+    String password,
+    EmailSignInFormType formType,
+    bool isLoading,
+    bool submitted,
+  }) {
+    return EmailSignInModel(
+      email: email ?? this.email,
+      password: password ?? this.password,
+      formType: formType ?? this.formType,
+      isLoading: isLoading ?? this.isLoading,
+      submitted: submitted ?? this.submitted,
+    );
+  }
 }
