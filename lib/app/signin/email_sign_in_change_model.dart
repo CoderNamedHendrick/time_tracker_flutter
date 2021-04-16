d8bc4a7610fefeae82a03991e1bdfd8fdab6a086
Hendrick <sebastinesoacatp@gmail.com>
2021-04-16 03:25:31 +0100

    State management with ChangeNotifier

---
+++ lib/app/signin/email_sign_in_change_model.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/foundation.dart';
+import 'package:time_tracker_flutter/app/signin/email_sign_in_model.dart';
+import 'package:time_tracker_flutter/app/signin/validators.dart';
+
+class EmailSignInChangeModel with EmailAndPasswordValidators, ChangeNotifier{
+  EmailSignInChangeModel({
+    this.email = '',
+    this.password = '',
+    this.formType = EmailSignInFormType.signIn,
+    this.isLoading = false,
+    this.submitted = false,
+  });
+  final String email;
+  final String password;
+  final EmailSignInFormType formType;
+  final bool isLoading;
+  final bool submitted;
+
+  String get primaryButtonText {
+    return formType == EmailSignInFormType.signIn
+        ? 'Sign in'
+        : 'Create an account';
+  }
+
+  String get secondaryButtonText {
+    return formType == EmailSignInFormType.signIn
+        ? 'Need an account? Register'
+        : 'Have an account? Sign in';
+  }
+
+  bool get canSubmit {
+    return emailValidator.isValid(email) &&
+        passwordValidator.isValid(password) &&
+        !isLoading;
+  }
+
+  String get passwordErrorText {
+    bool showErrorText = submitted && !passwordValidator.isValid(password);
+    return showErrorText ? invalidPasswordErrorText : null;
+  }
+
+  String get emailErrorText {
+    bool showErrorText = submitted && !emailValidator.isValid(email);
+    return showErrorText ? invalidEmailErrorText : null;
+  }
+
+  EmailSignInChangeModel copyWith({
+    String email,
+    String password,
+    EmailSignInFormType formType,
+    bool isLoading,
+    bool submitted,
+  }) {
+    return EmailSignInChangeModel(
+      email: email ?? this.email,
+      password: password ?? this.password,
+      formType: formType ?? this.formType,
+      isLoading: isLoading ?? this.isLoading,
+      submitted: submitted ?? this.submitted,
+    );
+  }
+}
