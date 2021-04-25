95c6a80c7b565c37a0fb12bf455f5493f45bf3d8
Hendrick <sebastinesoacatp@gmail.com>
2021-04-25 15:19:15 +0100

    Refactoring database model class to read data from firestore

--- lib/app/home/models/job.dart
+++ lib/app/home/models/job.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/foundation.dart';
 
 class Job {
@@ -6,7 +5,19 @@ class Job {
   final String name;
   final int ratePerHour;
 
-  Map<String, dynamic> toMap(){
+  factory Job.fromMap(Map<String, dynamic> data) {
+    if (data == null) {
+      return null;
+    }
+    final String name = data['name'];
+    final int ratePerHour = data['ratePerHour'];
+    return Job(
+      name: name,
+      ratePerHour: ratePerHour,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
     return {
       'name': name,
       'ratePerHour': ratePerHour,
