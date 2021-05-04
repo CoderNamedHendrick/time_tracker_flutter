b066c69325e80c0b5d87195e228267d17723ffd4
Hendrick <sebastinesoacatp@gmail.com>
2021-05-05 00:03:38 +0100

    Improving jobs page UI

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -2,15 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter/app/home/jobs/add_job_page.dart';
+import 'package:time_tracker_flutter/app/home/jobs/job_list_tile.dart';
 import 'package:time_tracker_flutter/app/home/models/job.dart';
 import 'package:time_tracker_flutter/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker_flutter/common_widgets/show_exception_alert_dialog.dart';
 import 'package:time_tracker_flutter/services/auth.dart';
 import 'package:time_tracker_flutter/services/database.dart';
 
-
 class JobsPage extends StatelessWidget {
-
   Future<void> _signOut(BuildContext context) async {
     try {
       final auth = Provider.of<AuthBase>(context, listen: false);
@@ -28,11 +27,10 @@ class JobsPage extends StatelessWidget {
       defaultActionText: 'Logout',
       cancelActionText: 'Cancel',
     );
-    if (didRequestSignOut == true){
+    if (didRequestSignOut == true) {
       _signOut(context);
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -64,10 +62,15 @@ class JobsPage extends StatelessWidget {
     final database = Provider.of<Database>(context, listen: false);
     return StreamBuilder<List<Job>>(
       stream: database.jobsStream(),
-      builder: (context, snapshot){
-        if (snapshot.hasData){
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
           final jobs = snapshot.data;
-          final children = jobs.map((job) => Text(job.name)).toList();
+          final children = jobs
+              .map((job) => JobListTile(
+                    job: job,
+                    onTap: (() {}),
+                  ))
+              .toList();
           return ListView(children: children);
         }
         return Center(child: CircularProgressIndicator());
