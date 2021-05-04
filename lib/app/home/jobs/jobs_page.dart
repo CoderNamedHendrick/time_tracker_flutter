3dd5a5de7dab1764ecb2ec0ba789de0e294dbe9d
Hendrick <sebastinesoacatp@gmail.com>
2021-05-04 22:46:19 +0100

    Adding the job page

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter/app/home/jobs/add_job_page.dart';
 import 'package:time_tracker_flutter/app/home/models/job.dart';
 import 'package:time_tracker_flutter/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker_flutter/common_widgets/show_exception_alert_dialog.dart';
@@ -32,20 +33,6 @@ class JobsPage extends StatelessWidget {
     }
   }
 
-  Future<void> _createJob(BuildContext context) async{
-    try {
-      final database = Provider.of<Database>(context, listen: false);
-      await database.createJob(
-        Job(name: "Blogging", ratePerHour: 10)
-      );
-    } on FirebaseException catch(e){
-      showExceptionAlertDialog(
-        context,
-        title: 'Operation failed',
-        exception: e,
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,7 +55,7 @@ class JobsPage extends StatelessWidget {
       body: _buildContext(context),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => _createJob(context),
+        onPressed: () => AddJobPage.show(context),
       ),
     );
   }
