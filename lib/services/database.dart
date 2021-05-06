6b41c05c4b578adcac3f6aa942fb9df6b8d79f6c
Hendrick <sebastinesoacatp@gmail.com>
2021-05-06 23:49:23 +0100

    Deleting jobs

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,6 +5,7 @@ import 'package:time_tracker_flutter/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> setJob(Job job);
+  Future<void> deleteJob(Job job);
   Stream<List<Job>> jobsStream();
 }
 
@@ -17,6 +18,7 @@ class FirestoreDatabase implements Database {
   final _service = FirestoreService.instance;
 
   // writing jobs to firestore(creating new jobs and updating existing jobs)
+  @override
   Future<void> setJob(Job job) => _service.setData(
         path: APIPath.job(
           uid,
@@ -25,7 +27,12 @@ class FirestoreDatabase implements Database {
         data: job.toMap(),
       );
 
+  @override
+  Future<void> deleteJob(Job job) => _service.deleteData(
+        path: APIPath.job(uid, job.id),
+      );
   // reading jobs from firestore
+  @override
   Stream<List<Job>> jobsStream() => _service.collectionStream(
         path: APIPath.jobs(uid),
         builder: (data, documentId) => Job.fromMap(data, documentId),
